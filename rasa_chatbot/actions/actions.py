4c9928b7caa14a85405242c986199d7fc3a75760
AlexandreLanglade <alexandre.langlade@outlook.fr>
2022-01-06 12:03:36 +0100

    clean model

--- rasa_chatbot/actions/actions.py
+++ rasa_chatbot/actions/actions.py
@@ -1,27 +1,99 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+import time
+from typing import Any, Dict, List, Text
+
+from ivy.ivy import IvyServer
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import AllSlotsReset
+from rasa_sdk.executor import CollectingDispatcher
+from stt_tts import Tts
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionIvy(IvyServer):
+    def __init__(self):
+        IvyServer.__init__(self, "ActionIvy")
+        self.start('127.255.255.255:2010')
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+class Maison(Action):
+    def name(self) -> Text:
+        return "action_maison"
+
+    def run(self):
+        """
+        , dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        """
+        connexion = ActionIvy()
+        time.sleep(1)
+        tts = Tts()
+        #slots = tracker.current_slot_values()
+        slots = {"action_maison":"éteint","piece":"salon","chauffage":None,"lumiere":"lumière","temperature":None}
+        if "allum" in slots["action_maison"]:
+            if slots["lumiere"] == "lumière":
+                if slots["piece"] == "salon":
+                    connexion.send_msg("MAISON device=salon action=on")
+                    tts.say("J'ai allumé la lumière du salon.")
+                elif slots["piece"] == "chambre":
+                    connexion.send_msg("MAISON device=chambre action=on")
+                    tts.say("J'ai allumé la lumière de la chambre.")
+                else:
+                    connexion.send_msg("MAISON device=salon action=on")
+                    connexion.send_msg("MAISON device=chambre action=on")
+                    tts.say("J'ai allumé les lumières.")
+            elif slots["chauffage"] == "chauffage":
+                connexion.send_msg("MAISON device=radiateur action=on")
+                tts.say("J'ai allumé le chauffage.")
+            else:
+                tts.say("Je n'ai pas compris.")
+        elif "étein" in slots["action_maison"]:
+            if slots["lumiere"] == "lumière":
+                if slots["piece"] == "salon":
+                    connexion.send_msg("MAISON device=salon action=off")
+                    tts.say("J'ai éteint la lumière du salon.")
+                elif slots["piece"] == "chambre":
+                    connexion.send_msg("MAISON device=chambre action=off")
+                    tts.say("J'ai éteint la lumière de la chambre.")
+                else:
+                    connexion.send_msg("MAISON device=salon action=off")
+                    connexion.send_msg("MAISON device=chambre action=off")
+                    tts.say("J'ai éteint les lumières.")
+            elif slots["chauffage"] == "chauffage":
+                connexion.send_msg("MAISON device=radiateur action=off")
+                tts.say("J'ai éteint le chauffage.")
+            else:
+                tts.say("Je n'ai pas compris.")
+        elif "met" in slots["action_maison"] or "règ" in slots["action_maison"]:
+            try:
+                temperature = int(float(slots["temperature"]))
+                connexion.send_msg("MAISON device=temperature action=" + str(temperature))
+                tts.say("Le chauffage est maintenant réglé sur " + str(temperature) + " degré.")
+            except:
+                tts.say("Je n'ai pas compris.")
+        else:
+            tts.say("Je n'ai pas compris.")
+
+        connexion.stop()
+        return [AllSlotsReset()]
+
+
+class ListeCourses(Action):
+    def name(self) -> Text:
+        return "action_liste_courses"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        return [AllSlotsReset()]
+
+
+class Message(Action):
+    def name(self) -> Text:
+        return "action_message"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        return [AllSlotsReset()]
